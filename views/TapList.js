052cb373eb1e4a8b5f4f2930317acaa0f717d6bc
Geoff Manning <gfm@alsoenergy.com>
2016-06-11 00:44:15 -0600

    scoreboard progress

--- views/TapList.js
+++ views/TapList.js
@@ -17,63 +17,127 @@ class TapList extends Component {
     **************************************************************/
    constructor (props) {
       super(props);
-      var ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+      var result = this.calculateScoreboard(this.props);
       this.state = {
-         data: ds.cloneWithRows(this.props.data.length ? this.props.data : [{ beer_name: 'No Data Beer' }]),
+         scoreboard: result,
+         dataSource: this.getDataSource(result.slice(0, 7))
       };
    }
 
    componentWillReceiveProps (nextProps) {
-      var ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+      var result = this.calculateScoreboard(nextProps, this.state.scoreboard);
+
+      // HACK: https://github.com/facebook/react-native/issues/5934
       this.setState({
-         data: ds.cloneWithRows(nextProps.data.length ? nextProps.data : [{ beer_name: 'No Data Beer' }]),
+         scoreboard: result,
+         dataSource: this.getDataSource([])
       });
+      this.setState({
+         dataSource: this.getDataSource(result.slice(0, 7))
+      });
+   }
+
+   /***************************************************************
+    * CALCULATIONS
+    **************************************************************/   
+   calculateScoreboard (props, scores) {
+      var scoreboard = props.data.map(beerFlow => {
+         return {
+            ...beerFlow,
+            score: this.calculateScore(beerFlow, scores)
+         };
+      });
+
+      scoreboard.sort((a, b) => {
+         return a.score < b.score ? 1 : a.score === b.score ? 0 : -1;
+      });
+      return scoreboard;
+   }
+
+   calculateScore (beerFlow, scores) {
+      if (!scores) {
+         return 0;
+      }
+      var previous = first(scores, 'beer_id', beerFlow.beer_id);
+      if (!previous) {
+         return 0;
+      }
+
+      if (String(previous.amount_remaining) === String(beerFlow.amount_remaining)) {
+         return previous.score;
+      }
+
+      var newScore = previous.score + ((previous.amount_remaining - beerFlow.amount_remaining) * 1000);
+      if (isNaN(newScore) || (!newScore && newScore !== 0)) {
+         return previous.score;
+      }
+      else {
+         return newScore;
+      }
+   }
+
+   getDataSource (data) {
+      var ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+      return ds.cloneWithRows(data);
    }
 
    /***************************************************************
     * RENDERING
     **************************************************************/
    render () {
-      var { data } = this.state;
+      var { dataSource } = this.state;
+
       return (
          <ListView
-            dataSource={data}
+            dataSource={dataSource}
             renderRow={this.renderRow} />
       );
    }
 
    renderRow (row) {
-      var beer, abv, summary, imageSource;
-      for (var i = 0; i < beers.length; i++) {
-         if (beers[i].id === row.beer_id) {
-            beer = beers[i];
-            break;
-         }
-      }
+      var beer, abv, score, summary, imageSource;
+      beer = first(beers, 'id', row.beer_id);
       if (!beer) {
-         summary = '';
+         summary = 's';
          imageSource = require('./images/trr.png');
       }
       else {
-         abv = beer.abv;
+         abv = beer.abv; // abv ? `${abv}%` : ''
          summary = beer.style;
          imageSource = { uri: beer.label_image.original };
       }
 
+      score = Math.round(row.score);
+      if ((!score && score !== 0) || isNaN(score)) {
+         score = 'NaN';
+      }
+      else {
+         score = String(score);
+      }
+
+      // <View style={styles.abv}>
+      //    <Text style={styles.abvText}>{score}</Text>
+      // </View>
       return (
          <View style={styles.row}>
             <Image style={styles.thumb} source={imageSource} />
             <View style={styles.right}>
                <Text style={styles.title}>{row.beer_name}</Text>
                <Text style={styles.summary}>{summary}</Text>
-               
-            </View>
-            <View style={styles.abv}>
-               <Text style={styles.abvText}>{abv ? `${abv}%` : ''}</Text>
+               <Text style={styles.abvText}>{score}</Text>
             </View>
          </View>
       );
    }
+}
+
+function first (list, prop, val) {
+   for (var i = 0; i < list.length; i++) {
+      if (list[i][prop] === val) {
+         return list[i];
+      }
+   }
+   return null;
 }
 
 /***************************************************************
@@ -95,6 +159,7 @@ const styles = StyleSheet.create({
    abvText: {
       color: '#FFFFFF',
       textAlign: 'right',
+      fontWeight: 'bold',
    },
    right: {
       flex: 1,
